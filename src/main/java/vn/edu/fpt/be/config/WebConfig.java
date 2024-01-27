10f92843a78c442a79d8027f384d85e9b0389e6a
ngoctb13 <ngoctb136@gmail.com>
2024-01-27 13:56:58 +0700

    update web cors config

--- src/main/java/vn/edu/fpt/be/config/WebConfig.java
+++ src/main/java/vn/edu/fpt/be/config/WebConfig.java
@@ -1,18 +1,18 @@
-//package vn.edu.fpt.be.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/api/**")
-//                .allowedOriginPatterns("*")  // Allow requests from your frontend origin
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(false)
-//                .maxAge(3600);
-//    }
-//}
+package vn.edu.fpt.be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")  // Allow requests from your frontend origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
