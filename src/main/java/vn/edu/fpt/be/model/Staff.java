a1a009633d75133138d49e9c6188251ec8dc9cd7
ngoctb13 <ngoctb136@gmail.com>
2024-02-04 11:20:41 +0700

    create staff service

---
+++ src/main/java/vn/edu/fpt/be/model/Staff.java
@@ -0,0 +1,25 @@
+package vn.edu.fpt.be.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Staff extends Model{
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
