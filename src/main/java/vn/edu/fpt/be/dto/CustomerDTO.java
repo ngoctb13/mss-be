560f42119058d9c3a829a7bc208c8b2f360ec6ff
ngoctb13 <ngoctb136@gmail.com>
2024-02-06 17:49:29 +0700

    update customer service

---
+++ src/main/java/vn/edu/fpt/be/dto/CustomerDTO.java
@@ -0,0 +1,21 @@
+package vn.edu.fpt.be.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vn.edu.fpt.be.model.enums.Status;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+    private Long id;
+    private String customerName;
+    private String phoneNumber;
+    private String address;
+    private String note;
+    private Status status;
+    private Long storeId;
+}
