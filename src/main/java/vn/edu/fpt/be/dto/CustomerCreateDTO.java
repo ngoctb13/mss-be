3256a7a147d3d8d21ad790dc82240edefaf91307
YOGA <hungtqhe161699@fpt.edu.vn>
2024-03-04 10:58:58 +0700

    update

--- src/main/java/vn/edu/fpt/be/dto/CustomerCreateDTO.java
+++ src/main/java/vn/edu/fpt/be/dto/CustomerCreateDTO.java
@@ -1,17 +1,29 @@
+
 package vn.edu.fpt.be.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerCreateDTO {
+    @NotNull(message = "Customer name cannot be null")
+    @Size(min = 1, message = "Customer name cannot be empty")
     private String customerName;
+
+    @NotNull(message = "Phone number cannot be null")
+    @Pattern(regexp = "[0-9]{10,15}", message = "Phone number must be between 10 and 15 digits")
     private String phoneNumber;
+
+    @Size(min = 1, message = "Address cannot be empty")
     private String address;
+
     private String note;
 }
--- src/main/java/vn/edu/fpt/be/dto/CustomerDTO.java
+++ src/main/java/vn/edu/fpt/be/dto/CustomerDTO.java
@@ -1,3 +1,4 @@
+
 package vn.edu.fpt.be.dto;
 
 import lombok.AllArgsConstructor;
@@ -6,6 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import vn.edu.fpt.be.model.Store;
 import vn.edu.fpt.be.model.enums.Status;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Min;
 
 @Data
 @Builder
@@ -13,10 +18,21 @@ import vn.edu.fpt.be.model.enums.Status;
 @NoArgsConstructor
 public class CustomerDTO {
     private Long id;
+
+    @NotNull(message = "Customer name cannot be null")
+    @Size(min = 1, message = "Customer name cannot be empty")
     private String customerName;
+
+    @NotNull(message = "Phone number cannot be null")
+    @Pattern(regexp = "[0-9]{10,15}", message = "Phone number must be between 10 and 15 digits")
     private String phoneNumber;
+
+    @Size(min = 1, message = "Address cannot be empty")
     private String address;
+
     private String note;
+
     private Double totalDebt;
+    @NotNull(message = "Status cannot be null")
     private Status status;
 }
