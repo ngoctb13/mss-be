9de8db72e1cd61592f9d6316ffee729c3444ac32
YOGA <hungtqhe161699@fpt.edu.vn>
2024-03-04 11:15:23 +0700

    update

--- src/main/java/vn/edu/fpt/be/dto/SaleInvoiceDetailRequest.java
+++ src/main/java/vn/edu/fpt/be/dto/SaleInvoiceDetailRequest.java
@@ -1,3 +1,4 @@
+
 package vn.edu.fpt.be.dto;
 
 import lombok.AllArgsConstructor;
@@ -5,13 +6,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import vn.edu.fpt.be.model.Product;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class SaleInvoiceDetailRequest {
+    @NotNull(message = "Product ID cannot be null")
     private Long productId;
+
+    @Min(value = 0, message = "Quantity cannot be negative")
     private Double quantity;
+
+    @Min(value = 0, message = "Unit price cannot be negative")
     private Double unitPrice;
 }
--- src/main/java/vn/edu/fpt/be/dto/StoreCreateDTO.java
+++ src/main/java/vn/edu/fpt/be/dto/StoreCreateDTO.java
@@ -1,17 +1,26 @@
+
 package vn.edu.fpt.be.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import vn.edu.fpt.be.model.enums.Status;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class StoreCreateDTO {
+    @NotNull(message = "Store name cannot be null")
+    @Size(min = 1, message = "Store name cannot be empty")
     private String storeName;
+
+    @Size(min = 1, message = "Address cannot be empty")
     private String address;
+
+    @Pattern(regexp = "[0-9]{10,15}", message = "Phone number must be between 10 and 15 digits")
     private String phoneNumber;
 }
