a6ed2b84189a4c9d1e730dbac2483b70e5dc7be5
ngoctb13 <ngoctb136@gmail.com>
2024-03-15 04:15:09 +0700

    fix

--- src/main/java/vn/edu/fpt/be/config/WebConfig.java
+++ src/main/java/vn/edu/fpt/be/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOrigins("http://localhost:3000") // Allowed origin(s)
+                .allowedOrigins("http://152.42.200.77") // Allowed origin(s)
 //                .allowedOrigins("http://mss.mom") // Allowed origin(s)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
                 .allowedHeaders("*") // Allowed headers
