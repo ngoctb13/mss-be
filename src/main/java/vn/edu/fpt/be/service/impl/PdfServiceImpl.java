df03fb8d7ebe35b382d037fb35767ef1214313e5
ngoctb13 <ngoctb136@gmail.com>
2024-03-23 20:54:03 +0700

    fix

--- src/main/java/vn/edu/fpt/be/service/impl/PdfServiceImpl.java
+++ src/main/java/vn/edu/fpt/be/service/impl/PdfServiceImpl.java
@@ -44,7 +44,7 @@ public class PdfServiceImpl implements PDFService {
     private final DebtPaymentHistoryService debtPaymentHistoryService;
     private final UserService userService;
 
-    private static final String FONT_FILE = "src/main/resources/Trirong-Regular.ttf";
+    private static final String FONT_FILE = "src/main/resources/templates/Trirong-Regular.ttf";
     @Override
     public ByteArrayInputStream generateInvoicePdf(Long saleInvoiceId) throws Exception {
 
