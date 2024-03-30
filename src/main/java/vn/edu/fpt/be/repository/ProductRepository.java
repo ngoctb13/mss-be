78ee4adbc84837e17a686166c36cf87536513f06
ngoctb13 <ngoctb136@gmail.com>
2024-03-30 22:59:29 +0700

    format datetime

--- src/main/java/vn/edu/fpt/be/repository/ProductRepository.java
+++ src/main/java/vn/edu/fpt/be/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package vn.edu.fpt.be.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import vn.edu.fpt.be.model.Product;
+import vn.edu.fpt.be.model.enums.Status;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByStoreId(Long storeId);
     List<Product> findByProductName(String productName);
     List<Product> findByStoreIdAndProductNameContaining(Long storeId, String nameInput);
+    @Query("SELECT p FROM Product p WHERE p.store.id = :storeId AND p.status = :status AND LOWER(p.productName) LIKE LOWER(CONCAT('%',:nameInput,'%'))")
+    List<Product> findByStoreIdAndStatusAndProductNameContaining(Long storeId, Status status, String nameInput);
+
 }
