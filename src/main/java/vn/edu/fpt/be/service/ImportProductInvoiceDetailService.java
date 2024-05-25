a4adf0a71e359977d9d522b48d4dfd61e9caa647
ngoctb13 <ngoctb136@gmail.com>
2024-05-25 13:57:49 +0700

    update

--- src/main/java/vn/edu/fpt/be/service/ImportProductInvoiceDetailService.java
+++ src/main/java/vn/edu/fpt/be/service/ImportProductInvoiceDetailService.java
@@ -3,10 +3,13 @@ package vn.edu.fpt.be.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import vn.edu.fpt.be.dto.ImportProductDetailRequest;
 import vn.edu.fpt.be.dto.ImportProductDetailResponse;
+import vn.edu.fpt.be.dto.SaleInvoiceDetailDTO;
+import vn.edu.fpt.be.dto.response.ImportInvoiceDetailResponse;
 import vn.edu.fpt.be.model.ImportProductInvoice;
 
 import java.util.List;
 
 public interface ImportProductInvoiceDetailService {
     void saveImportProductInvoiceDetail(List<ImportProductDetailRequest> request, ImportProductInvoice invoice);
+    List<ImportInvoiceDetailResponse> getDetailsOfImportInvoice(Long importInvoiceId);
 }
